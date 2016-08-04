ab230f6a0f769ff44533095c076e46a3256940db
iAnatoly <anatoly.ivanov@gmail.com>
2016-08-04 15:40:22 -0700

    implementing failure strategy

---
+++ PokemonGo.SimpleBot/APIFailureStrategy.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using PokemonGo.RocketAPI.Extensions;
+using PokemonGo.SimpleBot.Utils;
+using POGOProtos.Networking.Envelopes;
+
+namespace PokemonGo.SimpleBot
+{
+    class APIFailureStrategy: IApiFailureStrategy
+    {
+        // TODO: move the error tracking logic into this method
+        public Task<ApiOperation> HandleApiFailure(RequestEnvelope request, ResponseEnvelope response)
+        {
+            Randomization.RandomDelay(5000).Wait();
+            return Task.Run(() => ApiOperation.Abort);
+        }
+
+        public void HandleApiSuccess(RequestEnvelope request, ResponseEnvelope response)
+        {
+            Randomization.RandomDelay(500).Wait();
+        }
+    }
+}
